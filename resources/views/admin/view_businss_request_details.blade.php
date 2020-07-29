0fdbf33e3fb0e2b8b53486cf62d1e46fe9095efa
peekinternational <peekinternational@gmail.com>
2020-07-29 18:01:37 +0500

    emails

--- resources/views/admin/view_businss_request_details.blade.php
+++ resources/views/admin/view_businss_request_details.blade.php
@@ -58,7 +58,7 @@
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="#pablo">Timesheet Details</a>
+            <a class="navbar-brand" href="#pablo">Business Quotes Request Details</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
